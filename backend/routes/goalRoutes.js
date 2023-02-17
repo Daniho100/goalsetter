a017e47c86c40a6ecf472863af57cde34e094eab
Ojiaku Daniel <daniandre.dah@gmail.com>
2023-02-17 11:49:36 +0100

    frontend UI included

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalsController')
+const {protect} = require('../middlewares/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 // router.get('/', getGoals) 
 // router.post('/', setGoal)
 // router.put('/:id', updateGoal)
 // router.delete('/:id', deleteGoal)
-
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const { urlencoded } = require('express')
 const express = require('express')
+const { urlencoded } = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const PORT = process.env.PORT || 5000
@@ -11,7 +11,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorhandler)
 
